80c37626340f6cd750b7ab067cfa85664a608e5a
Andreas <andreas.kovalski@tu-dortmund.de>
2025-06-16 12:26:12 +0200

    Added GPL licence

--- Whi-l.hs
+++ Whi-l.hs
@@ -1,3 +1,21 @@
+{-
+CLI for the Whi-L interpreter and REPL
+Copyright (C) 2025  Andreas Kovalski
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-}
+
 import WhileModule
 import System.IO ( hGetContents, withFile, IOMode(ReadMode), isEOF )
 import System.Environment (getArgs)
